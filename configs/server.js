d180fc5db370aaa9a1234ea59f7bf6efa65a5202
Alejandro <amax-2019189@kinal.edu.gt>
2024-06-05 00:09:56 -0600

    Se cargo el login y el register

--- configs/server.js
+++ configs/server.js
@@ -3,11 +3,13 @@ import cors from 'cors'
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { dbConnection } from './mongo.js';
+import authRoutes from '../src/auth/auth.routes.js';
  
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.authPath = '/sistemaBancario/v1/auth'
 
         this.conectarDB();
         this.middlewares();
@@ -29,7 +31,7 @@ class Server {
  
    
     routes() {  
- 
+        this.app.use(this.authPath, authRoutes);
     };
  
     listen() {
--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -7,9 +7,56 @@ export const register = async (req, res) => {
         const { email, username, password} = req.body;
         const encryptPassword = bcryptjs.hashSync(password);
 
-        const user = await User.create()
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password: encryptPassword,
+        });
+
+        return res.status(200).json({
+            msg: "|-- user has been added to database --|",
+            userDetails: {
+                user: user.username,
+                password: user.password,
+                email: user.email
+            },
+        });
+
     } catch (e) {
         console.log(e);
         return res.status(500).send("Failed to register user");
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email: email.toLowerCase() });
+
+        if( !user){
+            return res
+                .status(400)
+                .send(`Wrong credentials, ${email} doesn´t exist in database`);
+        }
+
+        const validPassword = bcryptjs.compareSync(password, user.password);
+
+        if ( !validPassword ) {
+            return res.status( 400 ).send( "wrong password" );
+          }
+
+    const token = await generarJWT( user.id, user.email, user.username );
+
+    res.status( 200 ).json( {
+      msg: "Login Ok!!!",
+      userDetails: {
+        username: user.username,
+        token: token,
+      },
+    } );
+
+    } catch (e) {
+        res.status(500).send("Comuniquese con el administrador");
     }
 }
