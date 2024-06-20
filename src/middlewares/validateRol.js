a852af8fa54a96b7869446da84780f6de52122ae
Bryanth87 <tgbryan14@gmail.com>
2024-06-20 12:00:17 -0600

    Se realizo la validacion de rol

---
+++ src/middlewares/validateRol.js
@@ -0,0 +1,8 @@
+export const validRol = async (rol) => {
+    const validRol = ["gerente", "administrador", "caja"];
+    if (!validRol.incluides(rol)) {
+        return true;
+    } else{ 
+        return false;
+    }
+}
